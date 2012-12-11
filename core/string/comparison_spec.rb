d1ffe31bdc7bc17cf048a9be2856f90aa253031f
Josh Ballanco <jballanc@gmail.com>
2012-12-11 21:39:00 +0200

    String#<=> uses return value of other#to_str
    Commit r38044 updates behavior of String#<=> to account for the case
    where other defines #to_str. [Bug #7342]

--- core/string/comparison_spec.rb
+++ core/string/comparison_spec.rb
@@ -47,26 +47,50 @@ end
 # Note: This is inconsistent with Array#<=> which calls #to_ary instead of
 # just using it as an indicator.
 describe "String#<=>" do
-  it "returns nil if its argument does not provide #to_str" do
-    ("abc" <=> 1).should == nil
-    ("abc" <=> :abc).should == nil
-    ("abc" <=> mock('x')).should == nil
+  ruby_version_is ""..."2.0" do
+    it "returns nil if its argument does not provide #to_str" do
+      ("abc" <=> 1).should == nil
+      ("abc" <=> :abc).should == nil
+      ("abc" <=> mock('x')).should == nil
+    end
+
+    it "returns nil if its argument does not provide #<=>" do
+      obj = mock('x')
+      ("abc" <=> obj).should == nil
+    end
+
+    it "calls #to_str to convert the argument to a String and calls #<=> to compare with self" do
+      obj = mock('x')
+
+      # String#<=> merely checks if #to_str is defined on the object. It
+      # does not call the method.
+      obj.stub!(:to_str)
+
+      obj.should_receive(:<=>).with("abc").and_return(1)
+
+      ("abc" <=> obj).should == -1
+    end
   end
 
-  it "returns nil if its argument does not provide #<=>" do
-    obj = mock('x')
-    ("abc" <=> obj).should == nil
-  end
+  ruby_version_is "2.0" do
+    it "returns nil if its argument doesn not provide #to_str or #<=>" do
+      ("abc" <=> 1).should == nil
+      ("abc" <=> :abc).should == nil
+      ("abc" <=> mock('x')).should == nil
+    end
 
-  it "calls #to_str to convert the argument to a String and calls #<=> to compare with self" do
-    obj = mock('x')
+    it "uses the result of calling #to_str for comparison when #to_str is defined" do
+      obj = mock('x')
+      obj.should_receive(:to_str).and_return("xyz")
 
-    # String#<=> merely checks if #to_str is defined on the object. It
-    # does not call the method.
-    obj.stub!(:to_str)
+      ("abc" <=> obj).should == -1
+    end
 
-    obj.should_receive(:<=>).with("abc").and_return(1)
+    it "uses the result of calling  #<=> on the target when #<=> is defined but #to_str is not" do
+      obj = mock('x')
+      obj.should_receive(:<=>).and_return(-1)
 
-    ("abc" <=> obj).should == -1
+      ("abc" <=> obj).should == 1
+    end
   end
 end
